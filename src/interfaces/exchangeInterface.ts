4330eb7bbdd7ffeeb74aceadd62404850deaf120
Morris <pupugggg@DESKTOP-U37ML91>
2024-05-02 10:23:37 +0800

    add Exchange interface for currency exchange operation

--- src/interfaces/exchangeInterface.ts
+++ src/interfaces/exchangeInterface.ts
@@ -3,3 +3,7 @@ import {Currency} from "../constant/constant";
 export interface ExchangeRatio {
     getExchangeRatio(source: Currency, target: Currency): number;
 }
+
+export interface Exchange {
+    exchange(source: Currency, target: Currency, amount: number): string;
+}
