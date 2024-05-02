12c28537ecb2c7f2271ead5ffe1658d9584dc70a
Morris <pupugggg@DESKTOP-U37ML91>
2024-05-02 10:09:17 +0800

    add reusable constant for currency

---
+++ src/constant/constant.ts
@@ -0,0 +1,10 @@
+export enum Currency {
+    TWD = "TWD",
+    USD = "USD",
+    JPY = "JPY",
+}
+export const AVAILABLE_CURRENCIES: Array<string> = [
+    Currency.TWD,
+    Currency.JPY,
+    Currency.USD,
+];
