a8fcad6b98ec7d0cdec94719e43446b06ea7a4c2
Morris <pupugggg@DESKTOP-U37ML91>
2024-05-02 16:02:59 +0800

    exchange service unit test

--- test/exchange.test.ts
+++ test/exchange.test.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata";
 import {expect, test, describe} from "@jest/globals";
 import container from "../src/ioc_container";
-import {ExchangeRatio} from "../src/interfaces/exchangeInterface";
+import {Exchange, ExchangeRatio} from "../src/interfaces/exchangeInterface";
 import TYPES from "../src/constant/identifiers";
-import {Currency} from "../src/constant/constant";
+import {AVAILABLE_CURRENCIES, Currency} from "../src/constant/constant";
 
 describe("LocalExchangeRepository.getExchangeRatio()", () => {
     test("expect exchange ratio to be correct", () => {
@@ -48,5 +48,73 @@ describe("LocalExchangeRepository.getExchangeRatio()", () => {
                 Currency.TWD
             )
         ).toThrow("currency not found");
+    });
+});
+describe("ExchangeService.exchange()", () => {
+    test("expect exchanged currency amount to be correct", () => {
+        const LocalExchangeRepository = container.get<ExchangeRatio>(
+            TYPES.ExchangeRatio
+        );
+        const exchangeService = container.get<Exchange>(TYPES.Exchange);
+        const amount = 1234567;
+        AVAILABLE_CURRENCIES.forEach(source => {
+            AVAILABLE_CURRENCIES.forEach(target => {
+                const expectedNumber =
+                    Math.round(
+                        LocalExchangeRepository.getExchangeRatio(
+                            source as Currency,
+                            target as Currency
+                        ) *
+                            amount *
+                            100
+                    ) / 100;
+                const expectedResult = Intl.NumberFormat("en-us", {
+                    style: "currency",
+                    currency: "usd",
+                })
+                    .format(expectedNumber)
+                    .slice(1);
+                expect(
+                    exchangeService.exchange(
+                        source as Currency,
+                        target as Currency,
+                        amount
+                    )
+                ).toBe(expectedResult);
+            });
+        });
+    });
+    test("expect exchanged currency amount to be correct", () => {
+        const LocalExchangeRepository = container.get<ExchangeRatio>(
+            TYPES.ExchangeRatio
+        );
+        const exchangeService = container.get<Exchange>(TYPES.Exchange);
+        const amount = 1234567;
+        AVAILABLE_CURRENCIES.forEach(source => {
+            AVAILABLE_CURRENCIES.forEach(target => {
+                const expectedNumber =
+                    Math.round(
+                        LocalExchangeRepository.getExchangeRatio(
+                            source as Currency,
+                            target as Currency
+                        ) *
+                            amount *
+                            100
+                    ) / 100;
+                const expectedResult = Intl.NumberFormat("en-us", {
+                    style: "currency",
+                    currency: "usd",
+                })
+                    .format(expectedNumber)
+                    .slice(1);
+                expect(
+                    exchangeService.exchange(
+                        source as Currency,
+                        target as Currency,
+                        amount
+                    )
+                ).toBe(expectedResult);
+            });
+        });
     });
 });
